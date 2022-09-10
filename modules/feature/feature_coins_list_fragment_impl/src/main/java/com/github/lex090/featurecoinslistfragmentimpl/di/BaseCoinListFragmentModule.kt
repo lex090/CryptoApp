f35394b0cb3f081f24d9d7a760eaf2e5941c0ab8
Ilya Lisitsa <lex7555@yandex.ru>
2022-09-10 20:16:50 +0300

    Рефакторинг

--- modules/feature/feature_coins_list_fragment_impl/src/main/java/com/github/lex090/featurecoinslistfragmentimpl/di/BaseCoinListFragmentModule.kt
+++ modules/feature/feature_coins_list_fragment_impl/src/main/java/com/github/lex090/featurecoinslistfragmentimpl/di/BaseCoinListFragmentModule.kt
@@ -1,5 +1,6 @@
 package com.github.lex090.featurecoinslistfragmentimpl.di
 
+import androidx.recyclerview.widget.DiffUtil
 import com.github.lex090.coreapi.data.IMapper
 import com.github.lex090.coreapi.domain.IBaseUseCase
 import com.github.lex090.corenetworkapi.IRemoteNetworkServiceGenerator
@@ -11,6 +12,10 @@ import com.github.lex090.featurecoinslistfragmentimpl.data.services.CoinsNetwork
 import com.github.lex090.featurecoinslistfragmentimpl.domain.ICoinsRepository
 import com.github.lex090.featurecoinslistfragmentimpl.domain.entity.CoinsList
 import com.github.lex090.featurecoinslistfragmentimpl.domain.usecases.GetCoinsListUseCase
+import com.github.lex090.featurecoinslistfragmentimpl.presentation.view.DisplayableItem
+import com.github.lex090.featurecoinslistfragmentimpl.presentation.view.adapters.CoinListItemAdapterFactoryImpl
+import com.github.lex090.featurecoinslistfragmentimpl.presentation.view.adapters.ICoinListItemAdapterFactory
+import com.github.lex090.featurecoinslistfragmentimpl.presentation.view.diffutil.CoinListDiffUtilCallBack
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
@@ -42,6 +47,14 @@ interface CoinListFragmentBindsModule {
     fun bind_CoinMapperImpl_to_IMapper(
         mapper: CoinMapperImpl
     ): IMapper<List<@JvmSuppressWildcards CoinResponse>, CoinsList>
+
+    @Binds
+    fun bind_CoinListItemAdapterFactoryImpl_to_ICoinListItemAdapterFactory(
+        coinListItemAdapterFactoryImpl: CoinListItemAdapterFactoryImpl
+    ): ICoinListItemAdapterFactory
+
+    @Binds
+    fun a(coinListDiffUtilCallBack: CoinListDiffUtilCallBack): DiffUtil.ItemCallback<DisplayableItem>
 }
 
 @Module
---
+++ modules/feature/feature_coins_list_fragment_impl/src/main/java/com/github/lex090/featurecoinslistfragmentimpl/presentation/view/diffutil/CoinListDiffUtilCallBack.kt
@@ -0,0 +1,45 @@
+package com.github.lex090.featurecoinslistfragmentimpl.presentation.view.diffutil
+
+import android.annotation.SuppressLint
+import androidx.recyclerview.widget.DiffUtil
+import com.github.lex090.featurecoinslistfragmentimpl.presentation.view.CoinUiEntity
+import com.github.lex090.featurecoinslistfragmentimpl.presentation.view.DisplayableItem
+import javax.inject.Inject
+
+class CoinListDiffUtilCallBack @Inject constructor() : DiffUtil.ItemCallback<DisplayableItem>() {
+    override fun areItemsTheSame(oldItem: DisplayableItem, newItem: DisplayableItem): Boolean {
+        return when {
+            oldItem is CoinUiEntity && newItem is CoinUiEntity -> {
+                oldItem.name == newItem.name
+            }
+            else -> false
+        }
+    }
+
+    @SuppressLint("DiffUtilEquals")
+    override fun areContentsTheSame(
+        oldItem: DisplayableItem,
+        newItem: DisplayableItem
+    ): Boolean {
+        return when {
+            oldItem is CoinUiEntity && newItem is CoinUiEntity -> {
+                oldItem == newItem
+            }
+            else -> false
+        }
+    }
+
+    override fun getChangePayload(oldItem: DisplayableItem, newItem: DisplayableItem): Any? {
+        return when {
+            oldItem is CoinUiEntity && newItem is CoinUiEntity -> {
+                if (oldItem.isFavorite != newItem.isFavorite) {
+                    newItem.isFavorite
+                } else {
+                    super.getChangePayload(oldItem, newItem)
+                }
+            }
+            else -> super.getChangePayload(oldItem, newItem)
+
+        }
+    }
+}
